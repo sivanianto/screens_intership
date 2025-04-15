d745913558548c0f3cdf84ba3dd6304d3b8243c3
Shivani <shivani@Shivanis-MacBook-Air.local>
2025-04-15 11:13:28 +0530

    "fourth commit"
    This reverts commit 42956c1accd2eb7978769fb2726b5426596cf92c.

--- bankapp/lib/pages/bottomnavbar.dart
+++ bankapp/lib/pages/bottomnavbar.dart
@@ -6,61 +6,76 @@ import 'package:bankapp/pages/statistics.dart';
 import 'package:bankapp/pages/settings.dart';
 
 class BottomNavBar extends StatefulWidget {
-   const BottomNavBar({
-  super.key,
-  required void Function(int index) onTap,
-       required int currentIndex,
-});
+  const BottomNavBar({
+    super.key,
+    required void Function(int index) onTap,
+    required int currentIndex,
+  });
 
- @override
-State<BottomNavBar> createState() => _BottomNavBarState();
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
 
-int _currentIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    MyCardPage(),
+    StatisticsPage(),
+    SettingsPage(),
+  ];
 
-final List<Widget> _pages = [
-     HomePage(),
-  MyCardPage(),
-StatisticsPage(),
-     SettingsPage(),
-];
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-    backgroundColor: ColorConstants.background,
-body: _pages[_currentIndex],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorConstants.background,
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         elevation: 8,
-  backgroundColor: ColorConstants.bottomnavbar,
-     selectedItemColor: ColorConstants.button,
-       unselectedItemColor: ColorConstants.silenttextcolor,
-type: BottomNavigationBarType.fixed,
-currentIndex: _currentIndex,
- selectedFontSize: 12,
-    unselectedFontSize: 12,
-         onTap: (index) {
-  setState(() {
-         _currentIndex = index;
-});
-},
- items: [
-  BottomNavigationBarItem(
- icon: Icon(Icons.home),
-label: 'Home'),
- BottomNavigationBarItem(icon: Icon(Icons.wallet),
-label: 'My Card'),
-BottomNavigationBarItem(
-  icon: Icon(Icons.bar_chart),
-        label: 'Statistics',
-     ),
-        BottomNavigationBarItem(
- icon: Icon(Icons.settings),
- label: 'Settings'),
-    ],
-  ),
- );
+        backgroundColor: ColorConstants.bottomnavbar,
+        selectedItemColor: ColorConstants.button,
+        unselectedItemColor: ColorConstants.silenttextcolor,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+              margin: EdgeInsets.only(top: 12),
+              child: Icon(Icons.home),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              margin: EdgeInsets.only(top: 12),
+              child: Icon(Icons.wallet),
+            ),
+            label: 'My Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              margin: EdgeInsets.only(top: 12),
+              child: Icon(Icons.bar_chart),
+            ),
+            label: 'Statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              margin: EdgeInsets.only(top: 12),
+              child: Icon(Icons.settings),
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
--- bankapp/lib/pages/mycard.dart
+++ bankapp/lib/pages/mycard.dart
@@ -5,152 +5,163 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:bankapp/colors/colors.dart';
 
 class MyCardPage extends StatefulWidget {
- const MyCardPage({super.key});
+  const MyCardPage({super.key});
 
   @override
-   State<MyCardPage> createState()=> _MyCardPageState();
+  State<MyCardPage> createState() => _MyCardPageState();
 }
 
 class _MyCardPageState extends State<MyCardPage> {
-double _sliderValue = 4600;
+  double _sliderValue = 4600;
 
-@override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-backgroundColor: ColorConstants.background,
+      backgroundColor: ColorConstants.background,
       appBar: AppBar(
         backgroundColor: ColorConstants.background,
-     elevation: 0,
-        leading: IconButton(icon: Icon(Icons.arrow_back,
-          color: Colors.white),
-          onPressed: () {},
-      ),
-        title: Text("My Cards",style: GoogleFonts.poppins(
-      color: Colors.white,
-              fontSize: 18,
-     fontWeight: FontWeight.w600,),),
-      centerTitle: true,
-    actions: [
-  Padding(padding: EdgeInsets.only(right: 16.0),
-          child: Icon(Icons.add, color: Colors.white),
+        surfaceTintColor: ColorConstants.background,
+        automaticallyImplyLeading: false,
+
+        elevation: 0,
+        title: Text(
+          "My Cards",
+          style: GoogleFonts.poppins(
+            color: ColorConstants.textcolor,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
         ),
-     ],
-),
-body: SafeArea(
- child: SingleChildScrollView(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-     crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-     CardTile(),
-             SizedBox(height: 16),
-  _buildTransactionList(),
-              SizedBox(height: 16),
-                _buildSpendingLimitSlider(),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Icon(Icons.add, color: Colors.white),
+          ),
         ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CardTile(),
+            SizedBox(height: 16),
+            _buildTransactionList(),
+            SizedBox(height: 16),
+            _buildSpendingLimitSlider(),
+          ],
         ),
       ),
-    ),
- );
+    );
   }
 
   Widget _buildTransactionList() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-      TransactionTile(
-      title: "Apple Store",
-subtitle: "Entertainment",
-         amount: "- \$5.99",
-      icon: Icons.apple,
-      amountColor: Colors.redAccent,
+      children: [
+        TransactionTile(
+          title: "Apple Store",
+          subtitle: "Entertainment",
+          amount: "- \$5.99",
+          icon: Icons.apple,
+          amountColor: Colors.redAccent,
         ),
-    TransactionTile(
-  title: "Spotify",
-   subtitle: "Music",
- amount: "- \$12.99",
-      icon: Icons.music_note,
-      amountColor: Colors.redAccent,
+        SizedBox(height: 20),
+        TransactionTile(
+          title: "Spotify",
+          subtitle: "Music",
+          amount: "- \$12.99",
+          icon: Icons.music_note,
+          amountColor: Colors.redAccent,
         ),
-     TransactionTile(
-   title: "Grocery",
-         subtitle: "Shopping",
-   amount: "- \$88",
-      icon: Icons.shopping_cart,
-  amountColor: Colors.redAccent,
- ),
-  ],
-  );
+        SizedBox(height: 20),
+
+        TransactionTile(
+          title: "Grocery",
+          subtitle: "Shopping",
+          amount: "- \$88",
+          icon: Icons.shopping_cart,
+          amountColor: Colors.redAccent,
+        ),
+        SizedBox(height: 20),
+      ],
+    );
   }
 
   Widget _buildSpendingLimitSlider() {
- return Column(
- crossAxisAlignment: CrossAxisAlignment.start,
-   children: [
-   Text("Monthly spending limit",
-        style: GoogleFonts.poppins(
-         color: ColorConstants.textcolor,
-       fontSize: 16,
-  fontWeight: FontWeight.w600,
-     ),
-     ),
-        SizedBox(height: 20),
-     Container(
-      decoration: BoxDecoration(
-  color: Color(0xFF1E1E2D),
-          borderRadius: BorderRadius.circular(18),
-        ),
-    padding: EdgeInsets.all(20),
-       child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Text("Amount: \$${_sliderValue.toStringAsFixed(2)}",
-      style: GoogleFonts.poppins(
-         color: ColorConstants.textcolor,
-      fontSize: 14,
-     fontWeight: FontWeight.w500,
-         ),
-     ),
-       SizedBox(height: 4),
-      Slider(
-     value: _sliderValue,
-    min: 0,
-     max: 10000,
-        activeColor: Colors.white,
-        thumbColor: ColorConstants.button,
-inactiveColor: Colors.white24,
-      onChanged: (value) {
-         setState(() {
-     _sliderValue = value;
-              });
-            },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Monthly spending limit",
+          style: GoogleFonts.poppins(
+            color: ColorConstants.textcolor,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
           ),
-        Row(
-   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-children: [
-         Text("\$0",
-         style: GoogleFonts.poppins(
-       color: ColorConstants.textcolor,
-          fontSize: 12,
-             ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF1E1E2D),
+            borderRadius: BorderRadius.circular(18),
+          ),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Amount: \$${_sliderValue.toStringAsFixed(2)}",
+                style: GoogleFonts.poppins(
+                  color: ColorConstants.textcolor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-             Text("\$${_sliderValue.toStringAsFixed(0)}",
-      style: GoogleFonts.poppins(
-          color: ColorConstants.textcolor,
-             fontSize: 12,
-                ),
-                ),
-           Text("\$10,000",
-     style: GoogleFonts.poppins(
-          color: ColorConstants.textcolor,
-       fontSize: 12,
-      ),
+              SizedBox(height: 4),
+              Slider(
+                value: _sliderValue,
+                min: 0,
+                max: 10000,
+                activeColor: Colors.white,
+                thumbColor: ColorConstants.button,
+                inactiveColor: Colors.white24,
+                onChanged: (value) {
+                  setState(() {
+                    _sliderValue = value;
+                  });
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "\$0",
+                    style: GoogleFonts.poppins(
+                      color: ColorConstants.textcolor,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    "\$${_sliderValue.toStringAsFixed(0)}",
+                    style: GoogleFonts.poppins(
+                      color: ColorConstants.textcolor,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    "\$10,000",
+                    style: GoogleFonts.poppins(
+                      color: ColorConstants.textcolor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-           ],
-        ),
-       ],
-     ),
-   )
-  ];
+      ],
+    );
   }
 }
